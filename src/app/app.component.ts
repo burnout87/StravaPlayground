ad9369e64434d443a58fb92ad7a0560b090d3095
burnout87 <gabriele.barni@gmail.com>
2019-11-13 00:13:35 +0100

    authorize request method, failed because the access-control-allow-origin needs to be provided

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Response} from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'StravaPlayground';
+  urlAuthorization = 'http://www.strava.com/oauth/authorize?client_id=40265&response_type=code&redirect_uri=http://localhost:4200/exchange_token&approval_prompt=force&scope=read'
+
+  authorize() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          //resolve(JSON.parse(xhr.response))
+          console.log(xhr.response)
+        } else {
+          console.log(xhr.response)
+        }
+      }
+    }
+    xhr.open("GET", this.urlAuthorization, true);
+    xhr.send();
+  }
 }
