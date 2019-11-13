218a530a440b349a51ca0a8253f24f5e5b3d9abf
burnout87 <gabriele.barni@gmail.com>
2019-11-14 00:17:30 +0100

    not much

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {Response} from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -8,21 +7,23 @@ import {Response} from '@angular/http';
 })
 export class AppComponent {
   title = 'StravaPlayground';
-  urlAuthorization = 'http://www.strava.com/oauth/authorize?client_id=40265&response_type=code&redirect_uri=http://localhost:4200/exchange_token&approval_prompt=force&scope=read'
+  urlAuthorization = 'http://www.strava.com/oauth/authorize?client_id=40265&' +
+  'response_type=code&' +
+  'redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read';
 
   authorize() {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
       if (xhr.readyState === 4) {
         if (xhr.status === 200) {
-          //resolve(JSON.parse(xhr.response))
-          console.log(xhr.response)
+          // resolve(JSON.parse(xhr.response))
+          console.log(xhr.response);
         } else {
-          console.log(xhr.response)
+          console.log(xhr.response);
         }
       }
-    }
-    xhr.open("GET", this.urlAuthorization, true);
+    };
+    xhr.open('GET', this.urlAuthorization, true);
     xhr.send();
   }
 }
