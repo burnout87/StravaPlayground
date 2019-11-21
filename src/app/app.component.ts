c5634ac67866560d8d826d25eae22782a4913546
burnout87 <gabriele.barni@gmail.com>
2019-11-22 00:31:32 +0100

    not much

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,36 +9,20 @@ export class AppComponent {
   title = 'StravaPlayground';
   requestAuthorizationUrlAPI = 'http://localhost:8080/getAuthenticationUrl';
 
-  onAuthSuccess(authSuccessUrl) {    
-        window.onClose(authSuccessUrl);
-    }
-  
+  onLoadSuccess() {
+          console.log("to close the tab");
+      }
+
   authorize() {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = () => {
       if (xhr.readyState === 4) {
         if (xhr.status === 200) {
-<<<<<<< HEAD
-          window.open(xhr.response, "_blank");
-        } else {
           console.log(xhr.response);
+          var authWindow = window.open(xhr.response);
         }
-=======
-          // resolve(JSON.parse(xhr.response))
-          console.log(xhr.response);
-          authWindow = window.open(xhr.response, "_blank");
-          $.ajax({
-                  url: "/auth/google/",
-                  type: "POST",
-                  data: {username : uname, password: pass}                      
-                }).success(function(data){
-                    var redirectUrl = data.url; // or "/home" as you had mentioned
-                    onAuthSuccess(redirectUrl);
-                });
-          } else {
-            console.log(xhr.response);
-          }
->>>>>>> 43856190c4b9c2bc51cdab3acd4e25f8171e53b2
+      } else {
+          console.log("Error");
       }
     };
     xhr.open('GET', this.requestAuthorizationUrlAPI, true);
